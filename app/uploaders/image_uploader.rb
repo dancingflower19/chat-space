f830375d0bf29d1a9e9421bb7a7b08977f3e9bb3
dancingflower19 <myka0109@gmail.com>
2019-09-18 17:11:33 +0900

    updated files

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,10 +12,8 @@ include CarrierWave::MiniMagick
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
+
   process resize_to_fit: [800, 800]
-
-
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
