13c4276d0857502ee0cf70ba2099a9f6eba2c2aa
dancingflower19 <myka0109@gmail.com>
2019-09-25 14:47:34 +0900

    updated some files

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function(){
   function buildMessage(message) {
     var img = message.image ? `<img src= ${ message.image }>` : "";
     var html =`<div class="message" data-message_id=“${message.id}”>
@@ -18,33 +18,56 @@ $(function() {
                 ${img}
               </div>`
               return html;
-}
-
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action');
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
-  .done(function(message){
-    var html = buildMessage(message);
-    $('.messages').append(html)
-    $('.form__submit').prop('disabled', false);
-    $('form').get(0).reset();
-    $('.messages').animate({scrollTop: $(".messages")[0].scrollHeight}, 1500);
-  })
-  .fail(function(){
-    alert('メッセージの送信に失敗しました');
-  })
-  })
-　
-  function ScrollToNewMessage(){
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
   }
-});
+  function ScrollToNewMessage(){
+    $('.main__message').animate({scrollTop: $('.main__message')[0].scrollHeight}, 'fast');
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+          .done(function(message){
+              var html = buildMessage(message);
+            $('.messages').append(html)
+            $('.form__submit').prop('disabled', false);
+            $('form').get(0).reset();
+            $('.messages').animate({scrollTop: $(".messages")[0].scrollHeight}, 1500);
+          })
+          .fail(function(){
+            alert('メッセージの送信に失敗しました');
+          });
+  });
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: api/messages,
+        data: {id: last_message_id},
+        type: 'GET',
+        dataType: 'json'
+      })
+    }
+          .done(function(data) {
+              var insertHTML = '';
+            data.forEach(function(message){
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({scrollTop: $(".messages")[0].scrollHeight}, 1500);
+            })
+          .fail(function() {
+            alert('自動更新に失敗しました');
+          });
+}
+}
+)}
