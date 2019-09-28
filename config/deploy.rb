309119c412f518d20589c36a2c4e95b3398e9b66
dancingflower19 <myka0109@gmail.com>
2019-09-28 11:22:55 +0900

    updated git files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
-lock "~> 3.11.1"
+lock '~> 3.11.1'
 
-set :application, "chat-space"
-set :repo_url, "git@example.com:dancingflower19/chat-space.git"
+set :application, 'chat-space'
+set :repo_url, 'git@github.com:dancingflower19/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
